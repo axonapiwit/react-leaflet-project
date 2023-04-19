8b8f950336568cc33bdce8746d7d922891158ff1
axonapiwit <axon.apiwit@gmail.com>
2023-04-19 17:37:54 +0700

    improve uncomment code and init images

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -2,20 +2,8 @@ import React from "react";
 import "leaflet/dist/leaflet.css";
 import BaseMap from "./layers/BaseMap";
 import MapLocation from "./MapLocation";
-// import icon from "leaflet/dist/images/marker-icon.png";
-// import iconShadow from "leaflet/dist/images/marker-shadow.png";
-// import L from "leaflet"
 
 import { MapContainer } from "react-leaflet";
-// import jsonData from "../json/th.json";
-
-// let DefaultIcon = L.icon({
-//   iconUrl: icon,
-//   shadowUrl: iconShadow,
-//   iconSize: [25, 41],
-//   iconAnchor: [12.5, 20.5],
-// });
-// L.Marker.prototype.options.icon = DefaultIcon;
 
 const MapContent = () => {
 
@@ -31,7 +19,6 @@ const MapContent = () => {
       >
         <BaseMap />
         <MapLocation />
-        {/* <MapLocation jsonData={jsonData} /> */}
       </MapContainer>
     </div>
   );
